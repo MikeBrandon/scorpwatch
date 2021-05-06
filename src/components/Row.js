a2906be5276e9bb468f9e52daeec6cd5a1762e3e
Mike Brandon <bkb.braock@gmail.com>
2021-05-06 14:13:56 +0300

    Styled Rows Optimizes Image Loading

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,5 +1,6 @@
 import React, { useState , useEffect } from 'react';
 import axios from "../axios";
+import "../css/Row.css"
 
 function Row({ title , fetchUrl}) {
     const [movies, setMovies] = useState([]);
@@ -20,7 +21,12 @@ function Row({ title , fetchUrl}) {
 
             <div className="row_posters">
                 {movies.map(movie => (
-                    <img className={"row_poster"} src={`${imgBaseUrl}${movie.poster_path}`} alt={movie.name}/>
+                    <img
+                        key={movie.id}
+                        className={"row_poster"}
+                        src={`${imgBaseUrl}${movie.poster_path}`}
+                        alt={movie.name}
+                    />
                 ))}
             </div>
 
