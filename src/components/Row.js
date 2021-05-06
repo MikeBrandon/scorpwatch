f1bf39f78cec78b393d175256168bd9a07096f4b
Mike Brandon <bkb.braock@gmail.com>
2021-05-06 13:49:10 +0300

    Getting first two rows from TMDB API

---
+++ src/components/Row.js
@@ -0,0 +1,31 @@
+import React, { useState , useEffect } from 'react';
+import axios from "../axios";
+
+function Row({ title , fetchUrl}) {
+    const [movies, setMovies] = useState([]);
+    const imgBaseUrl = "https://image.tmdb.org/t/p/original"
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    },[fetchUrl]);
+
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+
+            <div className="row_posters">
+                {movies.map(movie => (
+                    <img className={"row_poster"} src={`${imgBaseUrl}${movie.poster_path}`} alt={movie.name}/>
+                ))}
+            </div>
+
+        </div>
+    )
+}
+
+export default Row;
